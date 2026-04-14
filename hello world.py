4de78cf0492535a5ef25b5459362e1b7f842c5a1
Tonny <ndaigaantonyngatia236@gmail.com>
2026-04-14 12:34:47 +0300

    first commit

---
+++ hello world.py
@@ -0,0 +1,14 @@
+#x=input("enter num1:")
+#y=input("enter num2:")
+#print(int(x)+int(y))
+#name = input("enter your name:")
+#print(len(name))
+#print(0b10)
+#x = int(input("enter a number:"))
+#t = int(input("enter a number:"))
+#print(x+t)
+num=input("enter a number")
+x=num[0]
+y=num[1]
+sum=int(x)+int(y)
+print(sum)

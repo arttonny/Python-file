e81d0754ac51b06fbe29d5226e5d60d0f7839056
Tonny <ndaigaantonyngatia236@gmail.com>
2026-07-13 17:31:35 +0300

    am back

--- string.py
+++ string.py
@@ -1,4 +1,4 @@
-name=input("enter your name")
-age =int(input("enter your age"))
+name=input("enter your name : ")
+age =int(input("enter your age : "))
 print(f"My name is{name}")
 print(len(name))

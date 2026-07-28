1772f6c7f33ef39c70c19e37f1ec69273f70578e
Tonny <ndaigaantonyngatia236@gmail.com>
2026-07-28 14:59:30 +0300

    am back

---
+++ for.py
@@ -0,0 +1,10 @@
+names  = ["toni", "shaz", "kim", "john", "jose"]
+for name in names:
+    print(name)
+num = [1,2, 3, 4, 5, 6, 7]
+nums = []
+for i in num:
+    print(i)
+    square = i**2
+    nums.append(square)
+print (nums)
